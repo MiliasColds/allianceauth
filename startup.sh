90bf6bb5fb5e46318dd1d990fc2c4556c09bebfa
Cloud User <centos@ip-172-31-17-187.ec2.internal>
2018-02-24 03:30:39 +0000

    update startup script to include celery workers

--- startup.sh
+++ startup.sh
@@ -7,8 +7,10 @@ python manage.py migrate
 yes yes | python manage.py evolve --hint --execute
 
 python manage.py shell < run_alliance_corp_update.py
-
+celery -A alliance_auth beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler --detach
+celery -A alliance_auth worker -l info --detach
 python manage.py celeryd &
 python manage.py celerybeat &
 
 python manage.py runserver 0.0.0.0:8000&
+
